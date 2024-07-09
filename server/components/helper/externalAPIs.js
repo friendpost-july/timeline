4ee2be56c982c0f10b7fef358324193d6ed8e4b9
Powar <shreekrushna.powar@hpe.com>
2024-07-09 19:43:24 +0530

    external api calls

---
+++ server/components/helper/externalAPIs.js
@@ -0,0 +1,29 @@
+const { friendsServiceAPI, userServiceAPI } = require("./constants");
+
+exports.getFriends = async (userId) => {
+  if (userId) {
+    try {
+      const response = await axios.get(
+        `${friendsServiceAPI}/friends/${userId}`
+      );
+      let listOfFriendsId = response.map((friend) => friend.friendId);
+      return listOfFriendsId;
+    } catch (error) {
+      return [];
+    }
+  }
+  return [];
+};
+
+exports.getDeactivatedUsers = async () => {
+  try {
+    const body = {
+      status: true,
+    };
+    const response = await axios.post(`${userServiceAPI}/filter`, body);
+    let listOfDeactivatedUsers = response.filter((user) => user.id);
+    return listOfDeactivatedUsers;
+  } catch (error) {
+    return [];
+  }
+};
