b1771cec1c995c191f56b31df488b46955d8384e
shreekrushnapowar <80472666+shreekrushnapowar@users.noreply.github.com>
2024-07-10 15:36:24 +0530

    Delete server/components/helper/helper.js

--- server/components/helper/helper.js
+++
@@ -1,6 +0,0 @@
-export default getAllFilteredPosts = (posts, deactivatedUser) => {
-  const newPosts = posts.filter(
-    (post) => !deactivatedUser.includes(post.userId)
-  );
-  return newPosts;
-};
